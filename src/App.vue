f2adf40863abd906e12b2ada717743f5ebc057cb
wujiawen <sam_day@126.com>
2017-10-09 16:51:09 +0800

    ajax 获取接口数据

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
+<!-- APP入口文件 -->
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scope>
+  @import "./style/style";
 </style>
