f8c5f4b4eee5f5e60e6454af4565262dc896e5ba
wujiawen <sam_day@126.com>
2017-10-10 15:12:39 +0800

    渲染列表和详细页面

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,12 +1,38 @@
 <template>
-  <div>index page</div>
+  <div>
+    <myHeader></myHeader>
+    <div class="article_list">
+      <ul>
+        <li v-for="i in list">
+          <time v-text="$utils.goodTime(i.create_at)"></time>
+          <router-link :to="'/content/'+i.id">
+            {{i.title}}
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <myFooter></myFooter>
+  </div>
 </template>
 <script>
+  import myHeader from '../components/header.vue'
+  import myFooter from '../components/footer.vue'
   export default {
+    components: {myHeader, myFooter},
+    data () {
+      return {
+        list: []
+      }
+    },
     created () {
-      this.$api.get('topics', null, r => {
-        console.log(r)
-      })
+      this.getData()
+    },
+    methods: {
+      getData () {
+        this.$api.get('topics', null, r => {
+          this.list = r.data
+        })
+      }
     }
   }
 </script>
